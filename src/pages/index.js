156dd2db73f390f29fc43f8b1d8d58750dc83850
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-02-03 21:19:34 -0800

    added link comonent

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <Link to="/blog/">blog page</Link>
+    </div>
+  )
 }
