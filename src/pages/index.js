625c6ad0a5bcbb527118c205d4cdc534bc135915
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-02-06 18:15:37 -0800

    added a styled component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { Button } from "../components/Button"
 
 export default function Home() {
   return (
     <Layout>
       <h1>Hello World!</h1>
+      <Button>Press me</Button>
     </Layout>
   )
 }
