ef87ddcb385312962db4628f27953818dcd7f28a
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-02-07 11:26:13 -0800

    update

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -2,15 +2,34 @@ import React from "react"
 import Header from "../examples/Header"
 import HeaderStatic from "../examples/HeaderStatic"
 import Layout from "../components/Layout"
+import { graphql } from "gatsby"
 
-const examples = () => {
+const examples = props => {
   return (
     <Layout>
       <h1>Hello! This is the examples page.</h1>
       <Header />
       <HeaderStatic />
+      <h5>Age: {props.data.site.info.person.age}</h5>
     </Layout>
   )
 }
+
+export const data = graphql`
+  {
+    site {
+      info: siteMetadata {
+        author
+        data
+        description
+        title
+        person {
+          name
+          age
+        }
+      }
+    }
+  }
+`
 
 export default examples
