c1966d04e776032eb15e154bfa616636b1ba6ce5
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-02-06 19:55:59 -0800

    GraphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,12 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Gatsby Hello World",
+    description: "tutorial",
+    author: "Abel Zambrano",
+    data: ["product 1", "product 2", "product 3"],
+    person: { name: "Nippy", age: 1 },
+  },
   plugins: [`gatsby-plugin-styled-components`],
 }
