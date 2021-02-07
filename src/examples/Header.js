2b4c27a93824cae0caf130e89d8423dd891aad4a
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-02-07 10:08:02 -0800

    update

--- src/examples/Header.js
+++ src/examples/Header.js
@@ -4,23 +4,37 @@ import { useStaticQuery, graphql } from "gatsby"
 const getData = graphql`
   {
     site {
-      siteMetadata {
+      info: siteMetadata {
         author
         data
         description
         title
+        person {
+          name
+          age
+        }
       }
     }
   }
 `
 
 const Header = () => {
-  const data = useStaticQuery(getData)
-  console.log(data)
+  const {
+    site: {
+      info: {
+        title,
+        person: { name },
+      },
+    },
+  } = useStaticQuery(getData)
 
   return (
     <div>
       <h1>This our GrapgQL example in Heading component.</h1>
+      {/* <h2>Title: {data.site.siteMetadata.title}</h2>
+      <h2>Author: {data.site.siteMetadata.author}</h2> */}
+      <h2>Title: {title}</h2>
+      <h2>Name: {name}</h2>
     </div>
   )
 }
