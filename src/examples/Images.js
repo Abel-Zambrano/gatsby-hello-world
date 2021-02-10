1ae537808e68be9f390b59ddf9f4aa653e168b44
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-02-09 21:26:22 -0800

    update

--- src/examples/Images.js
+++ src/examples/Images.js
@@ -1,11 +1,31 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import img from "../images/image-4.jpg"
+import "./images.css"
+
+const getImages = graphql`
+  {
+    fixed: file(relativePath: { eq: "image-3.jpg" }) {
+      childImageSharp {
+        fixed(width: 300, height: 400) {
+          src
+        }
+      }
+    }
+    fluid: file(relativePath: { eq: "image-2.jpg" }) {
+      childImageSharp {
+        fluid {
+          src
+        }
+      }
+    }
+  }
+`
 
 const Images = () => {
   return (
     <div>
-      <h1>Hello from images.</h1>
+      <h1 className="images-title">Hello from Images</h1>
       <section className="images">
         <article className="single-image">
           <h3>Basic Image</h3>
