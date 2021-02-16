55eda73150e02e417bb6a3d82ba64de687809abd
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2021-02-15 18:56:40 -0800

    update

--- src/templates/product-template.js
+++ src/templates/product-template.js
@@ -1,13 +1,54 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
+import Layout from "../components/Layout"
+import Image from "gatsby-image"
 
-const productTemplate = props => {
-  console.log(props)
-
+const productTemplate = ({
+  data: {
+    product: {
+      price,
+      title,
+      image: { fixed },
+      info: { info },
+    },
+  },
+}) => {
   return (
-    <div>
-      <h1>Hello from product template</h1>
-    </div>
+    <Layout>
+      <div style={{ textAlign: "center" }}>
+        <Link to="/products">back to products</Link>
+        <h1>single product : {title}</h1>
+      </div>
+      <section className="single-product">
+        <article>
+          <Image fixed={fixed} />
+          <h1>{title}</h1>
+          <h3>${price}</h3>
+        </article>
+        <article>
+          <p>{info}</p>
+          <button>add to cart</button>
+        </article>
+      </section>
+    </Layout>
   )
 }
+
+export const query = graphql`
+  query GetSingleProduct($slug: String) {
+    product: contentfulProduct(slug: { eq: $slug }) {
+      price
+      title
+      image {
+        fixed(width: 300) {
+          ...GatsbyContentfulFixed
+        }
+      }
+      info {
+        info
+      }
+    }
+  }
+`
 
 export default productTemplate
